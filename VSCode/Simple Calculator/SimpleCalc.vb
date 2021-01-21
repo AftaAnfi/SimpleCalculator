2cab276bff2396cbadade4fe2dd308af50ebe634
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-21 08:52:55 -0700

    Started calculator project

---
+++ VSCode/Simple Calculator/SimpleCalc.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+Option Strict On
+
+Module SimpleCalc
+    Dim firstnumber As Integer
+    Dim secondnumber As Integer
+    Dim userInput As String
+    Dim problem As Boolean = False
+
+    Sub Main()
+
+        Console.Title = "Calculator (Simple)"
+        Console.WriteLine("WELCOME TO CALCULATOR")
+        Console.WriteLine("")
+LineFirstNumberInput:
+
+        Console.WriteLine("Enter first number")
+
+
+
+
+
+    End Sub
+
+
+
+End Module
