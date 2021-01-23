efe48a2b444ff8df435e3e7cd5690cc2fd064c2c
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-23 14:11:33 -0700

    Finished SimpleCalculator

--- VSCode/Simple Calculator/SimpleCalc.vb
+++ VSCode/Simple Calculator/SimpleCalc.vb
@@ -5,7 +5,7 @@ Option Strict On
 'RCET0265
 'Spring 2021
 'Simple Calculator
-'https://github.com/rosstimo/myrepo.git
+'https://github.com/AftaAnfi/SimpleCalculator.git
 
 
 Module SimpleCalc
@@ -13,38 +13,10 @@ Module SimpleCalc
 
     Sub Main()
 
-        'class notes
-        'Dim countBy As Integer = 1
-        'Dim someText As String
-        ''For i = 0 To 10 Step countBy
-        ''    someText &= "*"
-        ''    Console.WriteLine(someText)
-        ''    If i >= 9 Then
-        ''        countBy = -1
-        ''    End If
-        ''Next
-
-        'Dim loopCount As Integer
-
-        'While loopCount < 10
-
-        '    Console.WriteLine(StrDup(loopCount, "*"))
-
-        '    loopCount += 1
-        'End While
-
-
-        'Console.Clear()
-
-
-
-
-
-
-
         Dim firstnumber As Integer
         Dim secondnumber As Integer
         Dim userInput As String
+        Dim userIntInput As Integer
 
         Console.Title = "Calculator (Simple)"
         Console.WriteLine("WELCOME TO CALCULATOR")
@@ -91,25 +63,67 @@ LineSecondInput:
             GoTo LineSecondInput
         End Try
 
-        Console.WriteLine($"{firstnumber} is your first number and {secondnumber} is your second number.")
-        System.Threading.Thread.Sleep(3000)
+LineResultStart:
+        Console.Clear()
+        Console.WriteLine("Which of the following operations would you like to do?")
+        Console.WriteLine($"1. Add ({firstnumber} + {secondnumber})")
+        Console.WriteLine($"2. Multiply ({firstnumber} * {secondnumber})")
+
+        userInput = Console.ReadLine()
+
+        Try
+            userIntInput = CInt(userInput)
+        Catch ex As Exception
+            Console.WriteLine("That was not 1 or 2, Press any key to continue...")
+            Console.ReadKey(False)
+            GoTo LineResultStart
+        End Try
+
+        Select Case userIntInput
+            Case 1
+                Console.WriteLine($"{firstnumber} + {secondnumber} = {firstnumber + secondnumber}")
+            Case 2
+                Console.WriteLine($"{firstnumber} * {secondnumber} = {firstnumber * secondnumber}")
+            Case Else
+                Console.WriteLine("That was not 1 or 2, Press any key to continue...")
+                Console.ReadKey(False)
+                GoTo LineResultStart
+        End Select
+
+
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey(False)
 
 
     End Sub
+
+    'class notes
+    'Dim countBy As Integer = 1
+    'Dim someText As String
+    ''For i = 0 To 10 Step countBy
+    ''    someText &= "*"
+    ''    Console.WriteLine(someText)
+    ''    If i >= 9 Then
+    ''        countBy = -1
+    ''    End If
+    ''Next
+
+    'Dim loopCount As Integer
+
+    'While loopCount < 10
+
+    '    Console.WriteLine(StrDup(loopCount, "*"))
+
+    '    loopCount += 1
+    'End While
+
+
+    'Console.Clear()
+
 
     'for i = 0 to 10 step 2
     'Console.WriteLine(i)
     'Next
     'Console.ReadLine()
-
-
-
-
-
-
-
-
-
-
 
 End Module
